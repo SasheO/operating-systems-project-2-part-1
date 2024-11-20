050372e2aefc4daecd62b8238918a9802db37c3b
Mezisashe Ojuba <mezisashe.ojuba@bison.howard.edu>
2024-11-20 14:33:58 +0000

    copied lab 3 solution, need to edit to fit requirements

--- shm_processes.c
+++ shm_processes.c
@@ -3,69 +3,130 @@
 #include  <sys/types.h>
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-void  ClientProcess(int []);
+void ChildProcess(int*, int*);
+void  ParentProcess(int*, int*);
+void  DepositMoney();
 
-int  main(int  argc, char *argv[])
-{
-     int    ShmID;
-     int    *ShmPTR;
-     pid_t  pid;
-     int    status;
+int  main(int  argc, char *argv[]){
+  int    BankAcctID;
+  int    *BankAcctPtr;
+  int    TurnID;
+  int    *TurnPtr;
+  pid_t  pid;
 
-     if (argc != 5) {
-          printf("Use: %s #1 #2 #3 #4\n", argv[0]);
-          exit(1);
-     }
+  // provision shared memory for BankAcctID and TurnID, see any errors in allocating shared memory
+  BankAcctID = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+  if (BankAcctID < 0) {
+      printf("*** shmget error ***\n");
+      exit(1);
+  }
+  TurnID = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+  if (TurnID < 0) {
+      printf("*** shmget error ***\n");
+      exit(1);
+  }
+  printf("Process has received a shared memory of two integers...\n");
 
-     ShmID = shmget(IPC_PRIVATE, 4*sizeof(int), IPC_CREAT | 0666);
-     if (ShmID < 0) {
-          printf("*** shmget error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has received a shared memory of four integers...\n");
+  // set BankAcctPtr and TurnPtr pointers to the shared memory provisioned
+  BankAcctPtr = (int *) shmat(BankAcctID, NULL, 0);
+  if (*BankAcctPtr == -1) {
+      printf("*** shmat error ***\n");
+      exit(1);
+  }
+  TurnPtr = (int *) shmat(TurnID, NULL, 0);
+  if (*TurnPtr == -1) {
+      printf("*** shmat error ***\n");
+      exit(1);
+  }
+  printf("Process has attached the shared memory...\n");
+  
 
-     ShmPTR = (int *) shmat(ShmID, NULL, 0);
-     if (*ShmPTR == -1) {
-          printf("*** shmat error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has attached the shared memory...\n");
+  *BankAcctPtr = 0; // initialize value to 0
+  *TurnPtr = 0; // initialize value to 0
 
-     ShmPTR[0] = atoi(argv[1]);
-     ShmPTR[1] = atoi(argv[2]);
-     ShmPTR[2] = atoi(argv[3]);
-     ShmPTR[3] = atoi(argv[4]);
-     printf("Server has filled %d %d %d %d in shared memory...\n",
-            ShmPTR[0], ShmPTR[1], ShmPTR[2], ShmPTR[3]);
+  printf("Orig Bank Account = %d\n", *BankAcctPtr);
 
-     printf("Server is about to fork a child process...\n");
-     pid = fork();
-     if (pid < 0) {
-          printf("*** fork error (server) ***\n");
-          exit(1);
-     }
-     else if (pid == 0) {
-          ClientProcess(ShmPTR);
-          exit(0);
-     }
+  pid = fork();
+  if (pid < 0) {
+      printf("*** fork error ***\n");
+      exit(1);
+  }
+  else if (pid == 0) {
+      ChildProcess(BankAcctPtr, TurnPtr);
+      // exit(0);
+  }
+  else{
+    ParentProcess(BankAcctPtr, TurnPtr);
+  }
 
-     wait(&status);
-     printf("Server has detected the completion of its child...\n");
-     shmdt((void *) ShmPTR);
-     printf("Server has detached its shared memory...\n");
-     shmctl(ShmID, IPC_RMID, NULL);
-     printf("Server has removed its shared memory...\n");
-     printf("Server exits...\n");
-     exit(0);
+  return 0;
 }
 
-void  ClientProcess(int  SharedMem[])
-{
-     printf("   Client process started\n");
-     printf("   Client found %d %d %d %d in shared memory\n",
-                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3]);
-     printf("   Client is about to exit\n");
+
+void  ChildProcess(int  *BankAcctPtr, int *TurnPtr){
+  srandom(time(NULL));
+  int sleep_time, account, balance_needed, indx;
+  for (indx=0; indx < 25; indx ++){ // loop 25 times as per the lab prompt
+    sleep_time = random()%5+1;
+    sleep(sleep_time); // sleep random amount of time between 1-5 seconds
+    while(*TurnPtr!=1);  // wait to get lock
+    account = *BankAcctPtr; // copy the in BankAccount to a local variable account
+    
+    // student tries to withdraw random amount of money if they have enough in their bank account
+    balance_needed = random()%50; 
+    printf("Poor Student needs $%d\n", balance_needed);
+    if (balance_needed<=account){
+      account -= balance_needed;
+      printf("Poor Student: Withdraws $%d / Balance = $%d\n", balance_needed, account);
+    }
+    else{
+      printf("Poor Student: Not Enough Cash ($%d)\n", account );
+    }
+
+    * BankAcctPtr = account; // update shared variable BankAcctPtr with locat account variable
+    * TurnPtr = 0; // release lock
+  }
+}
+
+void  ParentProcess(int * BankAcctPtr, int *TurnPtr){
+  srandom(time(NULL));
+  int sleep_time, indx;
+  int * accountPTR;
+  int account; // local variable for bank account amount
+  for (indx=0; indx < 25; indx ++){  // loop 25 times as per the lab prompt
+    accountPTR = &account; // accountPTR pointer holds the address of account integer (effectively points to accout)
+    sleep_time = random()%5+1;
+    sleep(sleep_time); // sleep random amount of time between 1-5 seconds
+    while(*TurnPtr!=0); // wait to get lock
+    account = *BankAcctPtr; // copy the in BankAccount to a local variable account
+    if (account <= 100){
+      DepositMoney(accountPTR); // edits account variable using its pointer
+      *BankAcctPtr = account; // update shared variable BankAcctPtr with locat account variable
+    }
+    else{
+      printf("Dear old Dad: Thinks Student has enough Cash ($%d)\n", account);
+    }
+    *TurnPtr = 1; // release lock
+  }
+}
+
+void DepositMoney(int *accountPTR){
+  /*
+  Takes in int accountPTR pointer, generates a number <100.
+  if random number is even, it modifies the value in accountPTR and prints message.
+  otherwise prints message
+  */
+  srandom(time(NULL));
+  int deposit_amount = random()%100;
+  if (deposit_amount%2==0){
+    * accountPTR += deposit_amount;
+    printf("Dear old Dad: Deposits $%d / Balance = $%d\n", deposit_amount, *accountPTR);
+  }
+  else{
+    printf("Dear old Dad: Doesn't have any money to give\n");
+  }
 }
